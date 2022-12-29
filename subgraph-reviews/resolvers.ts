179c254a015c599747b05294bb5d0b584fb35f0d
levblanc <levblanc@gmail.com>
2022-12-29 10:43:52 +0800

    Add reviews subgraph

---
+++ subgraph-reviews/resolvers.ts
@@ -0,0 +1,21 @@
+const resolvers = {
+  Query: {
+    latestReviews: (_, __, { dataSources }) => {
+      return dataSources.reviewsAPI.getLatestReviews();
+    },
+  },
+  Mutation: {
+    submitReview: (_, { locationReview }, { dataSources }) => {
+      const newReview =
+        dataSources.reviewsAPI.submitReviewForLocation(locationReview);
+      return {
+        code: 200,
+        success: true,
+        message: 'success',
+        locationReview: newReview,
+      };
+    },
+  },
+};
+
+export default resolvers;
